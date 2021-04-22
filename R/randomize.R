26433fc235592d23d0ef81e8ffd906b3e227745e
Akshay Sanghi <asanghi@stanford.edu>
2021-04-21 23:06:37 -0700

    Upload package

---
+++ R/randomize.R
@@ -0,0 +1,14 @@
+#' Randomize samples into two groups, one for graphcorrelation function and one for predictprotein function
+#'
+#' @param orderSamples a vector of sample IDs
+#' @return A data frame of sample IDs and their associated designated group
+#' @examples
+#' randomize(c("1","2","3"))
+#' randomize(data/orderSamples.rda)
+
+randomize <- function(orderSamples){
+  sample_samples <- sample(orderSamples,round(length(orderSamples)/2))
+  test_samples <- prob::setdiff(orderSamples,sample_samples)
+  randomized_samples <- dplyr::bind_rows(dplyr::tibble(sample=sample_samples,randomized="graph"),dplyr::tibble(sample=test_samples,randomized="test"))
+  randomized_samples
+}
