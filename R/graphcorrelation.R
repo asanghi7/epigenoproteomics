b56ea8f74d0c5172201402b8c1e184b506d5df26
Akshay Sanghi <asanghi@stanford.edu>
2021-10-28 15:49:43 -0700

    Updated variable name and input description

--- R/graphcorrelation.R
+++ R/graphcorrelation.R
@@ -3,7 +3,7 @@
 #' Calculate epigenoproteomic correlations
 #'
 #' @param Peak_annotation A tibble data frame that lists assigned genes to peaks and their status as differential features
-#' @param PeakCount A tibble data frame that lists assigned genes to peaks and their accessibility counts for all samples. Sample columns have the same order as TranscriptCount and ProteinCount
+#' @param PeakCount A tibble data frame that lists assigned genes to peaks and their accessibility counts for all samples. Sample columns have the same order as TranscriptCount and ProteinCount. Make sure to use same column names for annotations.
 #' @param TranscriptCount A tibble data frame that lists genes and their RNA expression in log transformation of transcript per million units for all samples. Sample columns have the same order as PeakCount and ProteinCount
 #' @param ProteinCount A tibble data frame that lists genes and their protein expression in log transformation of normalized PSM counts for all samples. Sample columns have the same order as PeakCount and TranscriptCount
 #' @param orderSamples A vector of sample IDs in the order of the sample columns in Count data frames (i.e. PeakCount, TranscriptCount, and ProteinCount)
@@ -15,20 +15,20 @@ graphcorrelation <- function(Peak_annotation,PeakCount,TranscriptCount,ProteinCo
   PeakGene_xor_values <- dplyr::tibble()
   for(p in 1:nrow(Peak_annotation)){
     gene <- unlist(unname(Peak_annotation[p,1]))
-    peakName <- unlist(unname(Peak_annotation[p,2]))
+    rowName <- unlist(unname(Peak_annotation[p,2]))
     status <- unlist(unname(Peak_annotation[p,3]))
     transcript_count <- c()
     protein_count <- c()
     peak_count <- c()
     transcript_count <- unlist(unname(dplyr::filter(TranscriptCount,Gene==gene)[1,2:ncol(TranscriptCount)]))
     protein_count <- unlist(unname(dplyr::filter(ProteinCount,Gene==gene)[1,2:ncol(ProteinCount)]))
-    peak_count <- unlist(unname(dplyr::filter(PeakCount,Gene==gene,peakName==peakName)[1,3:ncol(PeakCount)]))
+    peak_count <- unlist(unname(dplyr::filter(PeakCount,Gene==gene,peakName==rowName)[1,3:ncol(PeakCount)]))
     cor_transProt_Peak <- dplyr::inner_join(dplyr::bind_cols(dplyr::tibble(transcript=transcript_count),dplyr::tibble(peak=peak_count),dplyr::tibble(protein=protein_count,samples=orderSamples)),dplyr::tibble(samples=graphSamples))
     trans_prot_xor <- c()
     trans_peak_xor <- c()
     trans_peak_xor <- stats::cor(cor_transProt_Peak$transcript,cor_transProt_Peak$peak,method="spearman")
     trans_prot_xor <- stats::cor(cor_transProt_Peak$transcript,cor_transProt_Peak$protein,method="spearman")
-    PeakGene_xor_values <- dplyr::bind_rows(PeakGene_xor_values,dplyr::tibble(Gene=gene,PeakName=peakName,Status=status,TranscriptPeakXor=trans_peak_xor,TranscriptProteinXor=trans_prot_xor))
+    PeakGene_xor_values <- dplyr::bind_rows(PeakGene_xor_values,dplyr::tibble(Gene=gene,PeakName=rowName,Status=status,TranscriptPeakXor=trans_peak_xor,TranscriptProteinXor=trans_prot_xor))
   }
   PeakGene_xor_values
 }
